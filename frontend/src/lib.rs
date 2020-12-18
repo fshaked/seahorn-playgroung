f0f1f67533b3a0a3d2459edf06c8df5ef3ca8f82
Shaked Flur <sflur@google.com>
2020-12-18 10:15:42 +0000

    Make lto work

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,15 +1,23 @@
-// extern "C" {
-//     fn __VERIFIER_error();
-// }
-#[no_mangle]
-fn __VERIFIER_error() {
+use core::panic::PanicInfo;
+
+extern {
+    fn __VERIFIER_error() -> !;
+    fn __VERIFIER_assume(pred: i32);
 }
-pub fn sassert(cond : bool) {
-    if ! cond {
-        // unsafe {
-            __VERIFIER_error();
-        // }
+
+pub fn abort() -> ! {
+    unsafe { __VERIFIER_error(); }
+}
+
+pub fn assume(pred: bool) {
+    if ! pred {
+        unsafe { __VERIFIER_assume(0); }
     }
+}
+
+#[no_mangle]
+fn spanic(_info: &PanicInfo) -> ! {
+    abort();
 }
 
 pub trait NonDet {
@@ -18,15 +26,10 @@ pub trait NonDet {
 
 macro_rules! make_nondet {
     ($typ:ty, $ext:ident, $v:expr) => {
-        // extern "C" {
-            #[no_mangle]
-            fn $ext() -> $typ { $v }
-        // }
+        extern { fn $ext() -> $typ; }
         impl NonDet for $typ {
             fn seahorn_nondet() -> Self {
-                // unsafe {
-                    $ext()
-                // }
+                unsafe { $ext() }
             }
         }
     };
@@ -48,3 +51,133 @@ make_nondet!(isize, __VERIFIER_nondet_isize, 0);
 
 make_nondet!(f32, __VERIFIER_nondet_f32, 0.0);
 make_nondet!(f64, __VERIFIER_nondet_f63, 0.0);
+
+
+#[macro_export]
+macro_rules! assert {
+    ($cond:expr,) => { $crate::assert!($cond) };
+    ($cond:expr) => { $crate::assert!($cond, "assertion failed: {}", stringify!($cond)) };
+    ($cond:expr, $($arg:tt)+) => {{
+        if ! $cond {
+            let message = format!($($arg)+);
+            eprintln!("VERIFIER: panicked at '{}', {}:{}:{}",
+                      message,
+                      std::file!(), std::line!(), std::column!());
+            $crate::abort();
+        }
+    }}
+}
+
+#[macro_export]
+macro_rules! assert_eq {
+    ($left:expr, $right:expr) => {{
+        let left = $left;
+        let right = $right;
+        $crate::assert!(
+            left == right,
+            "assertion failed: `(left == right)` \
+             \n  left: `{:?}`,\n right: `{:?}`",
+            left,
+            right)
+    }};
+    ($left:expr, $right:expr, $fmt:tt $($arg:tt)*) => {{
+        let left = $left;
+        let right = $right;
+        $crate::assert!(
+            left == right,
+            concat!(
+                "assertion failed: `(left == right)` \
+                 \n  left: `{:?}`, \n right: `{:?}`: ", $fmt),
+            left, right $($arg)*);
+    }};
+}
+
+#[macro_export]
+macro_rules! assert_ne {
+    ($left:expr, $right:expr) => {{
+        let left = $left;
+        let right = $right;
+        $crate::assert!(
+            left != right,
+            "assertion failed: `(left != right)` \
+             \n  left: `{:?}`,\n right: `{:?}`",
+            left,
+            right)
+    }};
+    ($left:expr, $right:expr, $fmt:tt $($arg:tt)*) => {{
+        let left = $left;
+        let right = $right;
+        $crate::assert!(
+            left != right,
+            concat!(
+                "assertion failed: `(left != right)` \
+                 \n  left: `{:?}`, \n right: `{:?}`: ", $fmt),
+            left, right $($arg)*);
+    }};
+}
+
+#[macro_export]
+macro_rules! unreachable {
+    () => {
+        $crate::abort();
+    };
+}
+
+#[macro_export]
+macro_rules! assume {
+    ($cond:expr) => { $crate::assume($cond) };
+}
+
+macro_rules! nondet_impls {
+    ($($ty:ident, $value:tt;)*) => {
+        #[macro_export]
+        macro_rules! nondet {
+            $(
+                ($value) => {
+                    $ty::seahorn_nondet()
+                };
+            )*
+        }
+    };
+}
+nondet_impls! {
+    u32, 0u32;
+    u32, 1u32;
+    u32, 2u32;
+    u32, 3u32;
+    u32, 4u32;
+    u32, 5u32;
+    u32, 6u32;
+    u32, 7u32;
+    u32, 8u32;
+
+    i32, 0i32;
+    i32, 1i32;
+    i32, 2i32;
+    i32, 3i32;
+    i32, 4i32;
+    i32, 5i32;
+    i32, 6i32;
+    i32, 7i32;
+    i32, 8i32;
+
+    u64, 0u64;
+    u64, 1u64;
+    u64, 2u64;
+    u64, 3u64;
+    u64, 4u64;
+    u64, 5u64;
+    u64, 6u64;
+    u64, 7u64;
+    u64, 8u64;
+
+    i64, 0i64;
+    i64, 1i64;
+    i64, 2i64;
+    i64, 3i64;
+    i64, 4i64;
+    i64, 5i64;
+    i64, 6i64;
+    i64, 7i64;
+    i64, 8i64;
+}
--- try-frontend/src/main.rs
+++ try-frontend/src/main.rs
@@ -2,7 +2,67 @@ use seahorn;
 use seahorn::NonDet;
 
 fn main() {
+    // All the tests should be unsat.
+    // The feature "fail<n>" (e.g. "fail0") changes test<n> so that it should be
+    // sat.
+    test0();
+    test1();
+    test2();
+    test3();
+    test4();
+}
+
+#[cfg_attr(test, test)]
+fn test0() {
+    let mut a : i32 = 43;
+    #[cfg(not(feature = "fail0"))] { a -= 1; }
+    seahorn::assert!(a == 42);
+}
+
+#[cfg_attr(test, test)]
+fn test1() {
     let a : i32 = i32::seahorn_nondet();
-    let b = a;
-    seahorn::sassert(b >= 0);
+    #[cfg(not(feature = "fail1"))] { seahorn::assume!(a < i32::MAX); }
+    let b = a + 1;
+    seahorn::assert!(a != b);
+}
+
+#[cfg_attr(test, test)]
+fn test2() {
+    let a : i32 = i32::seahorn_nondet();
+    let mut b : i32 = i32::seahorn_nondet();
+    #[cfg(not(feature = "fail2"))] { seahorn::assume!(b == 0); }
+    if b > 0 {
+        seahorn::unreachable!();
+        b = 0;
+    }
+    let c = a + b;
+    seahorn::assert!(a == c);
+}
+
+#[cfg_attr(test, test)]
+fn test3() {
+    let a : i32 = i32::seahorn_nondet();
+    let mut r : i32 = 0;
+    #[cfg(not(feature = "fail3"))] { seahorn::assume!(a > 1); }
+    for _i in 0..a as i32 {
+        r += 1;
+    }
+    seahorn::assert!(r == a);
+}
+
+fn rec_f(c : i32) -> i32 {
+    if c > 0 {
+        rec_f(c - 1) + 1
+    } else {
+        0
+    }
+}
+
+#[cfg_attr(test, test)]
+fn test4() {
+    let a : i32 = i32::seahorn_nondet();
+    #[cfg(not(feature = "fail4"))] seahorn::assume!(a > 1);
+    let r : i32 = rec_f(a);
+    seahorn::assert!(r == a);
 }
